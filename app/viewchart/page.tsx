c84b9e7d3097bb3181341f582d9aef5ddf509875
maruf-rahman007 <s.marufrahman.g@gmail.com>
2024-11-28 04:03:23 +0600

    prod fix

--- app/viewchart/page.tsx
+++ app/viewchart/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 import { useQuery } from "react-query";
 import axios from "axios";
-import ChartCard from "../components/chart";
+import dynamic from 'next/dynamic';
 import { useEffect } from 'react';
 import { useRecoilState } from "recoil";
 import { alldata, alldatashadow } from "../store/atoms/atom";
 import { filterLastSevenDays } from "../lib/filterdata";
 import { fetchData } from "../lib/fetch";
-
+const ChartCard = dynamic(() => import("../components/chart"), { 
+    ssr: false 
+});
 export default function Analytics() {
     const [dataa, setData] = useRecoilState(alldata);
     const [showdata, setShowData] = useRecoilState(alldatashadow);
